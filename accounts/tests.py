a67c614b9566f00c902359e89940e7d527e2d0e1
Tomasz Rup <tomasz.r@elcar-gps.com.pl>
2014-10-26 15:18:28 +0100

    simple tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,7 +2,22 @@ from django.test import TestCase
 
 
 class AccountsViewsTestCase(TestCase):
+    fixtures = ['users_views_testdata.json']
+
     def test_index(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('users' in resp.context)
+        self.assertEqual([user.pk for user in resp.context['users']], [1])
+
+    def test_details(self):
+        resp = self.client.get('/view/1/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('user' in resp.context)
+        self.assertEqual(resp.context['user'].pk, 1)
+
+    def test_edit(self):
+        resp = self.client.get('/edit/1/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('user' in resp.context)
+        self.assertEqual(resp.context['user'].pk, 1)
