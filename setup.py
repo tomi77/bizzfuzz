d0f97b164071ea39e5d3e8b94727f726362fd326
Tomasz Rup <tomasz.r@elcar-gps.com.pl>
2014-10-27 07:54:35 +0100

    make coverage works

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="bizzfuzz",
+    author="Tomasz Jakub Rup",
+    author_email="tomasz.rup@gmail.com",
+    test_suite="bizzfuzz.runtests.runtests",
+)
