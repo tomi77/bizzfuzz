7d7cc21a50571acb8628765c56c201bf38df539f
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2015-12-23 21:59:56 +0100

    refactoring tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 
@@ -5,29 +6,39 @@ class AccountsViewsTestCase(TestCase):
     fixtures = ['users_views_testdata.yaml']
 
     def test_index(self):
-        resp = self.client.get('/')
+        url = reverse('index')
+        resp = self.client.get(url)
+
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('users' in resp.context)
         self.assertEqual([user.pk for user in resp.context['users']], [1, 2, 3, 4])
 
     def test_details(self):
-        resp = self.client.get('/view/1/')
+        url = reverse('view', args=[1])
+        resp = self.client.get(url)
+
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('user' in resp.context)
         self.assertEqual(resp.context['user'].pk, 1)
 
     def test_edit(self):
-        resp = self.client.get('/edit/1/')
+        url = reverse('edit', args=[1])
+        resp = self.client.get(url)
+
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('user' in resp.context)
         self.assertEqual(resp.context['user'].pk, 1)
 
     def test_add(self):
-        resp = self.client.get('/add/')
+        url = reverse('add')
+        resp = self.client.get(url)
+
         self.assertEqual(resp.status_code, 200)
 
     def test_delete_non_existent_user(self):
-        resp = self.client.get('/del/99/', follow=True)
+        url = reverse('del', args=[99])
+        resp = self.client.get(url, follow=True)
+
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('alert' in resp.context)
         self.assertIsNotNone(resp.context['alert'])
