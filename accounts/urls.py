638e71970617b02a7dce748e22db60e60a37be9b
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2015-12-23 14:59:07 +0100

    fix urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
                        url(r'^$', 'accounts.views.index', name='index'),
-                       url(r'^edit/([\d+])/$', 'accounts.views.edit', name='edit'),
-                       url(r'^view/([\d+])/$', 'accounts.views.view', name='view'),
-                       url(r'^del/([\d+])/$', 'accounts.views.delete', name='del'),
+                       url(r'^edit/(\d+)/$', 'accounts.views.edit', name='edit'),
+                       url(r'^view/(\d+)/$', 'accounts.views.view', name='view'),
+                       url(r'^del/(\d+)/$', 'accounts.views.delete', name='del'),
                        url(r'^add/$', 'accounts.views.add', name='add'))
